aa4496f45ca3d4ef6f66a4a0a9903e42b83e0e14
San-249 <maruthisanjeev2002@gmail.com>
2024-08-22 16:34:37 +0530

    first commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,36 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalratings: 0,
+
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      },
+
+      handledislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }));
+      },
+    };
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+      <div className='content-rating'>
+        <p> MOVIE: KAALA</p>
+        <div className='ratingButtons'>
+        <button className='likebutton'>Like({this.state.likes})</button>
+        <button className='likebutton'>Dislike({this.state.dislikes})</button>
+        <p>Total Ratings: {this.state.totalratings}</p>
+        </div>
+      </div>
      </>
     );
   }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'C:/Users/marut/OneDrive/Desktop/Webpages/rating page/content_rating/index.html', // Explicitly define the entry point
+    },
+  },
 })
